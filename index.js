05df946be1e35546b0c659bde5c29e831263920e
achdj <achilledjegnon@gmail.com>
2023-01-15 19:04:25 +0000

    update

--- index.js
+++ index.js
@@ -1,182 +1,82 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const { Router } = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User, Exercise } = require('./models/model');
 
-const router = Router();
-//db connexion
-const db = process.env['MONGO_URI'];
+const app = express();
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("Mongoose is connected")
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const Schema = mongoose.Schema;
-//user schema
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  log: [{
-    description: {
-      type: String,
-      required: true
-    },
-    duration: {
-      type: Number,
-      required: true
-    },
-    date: {
-      type: Date,
-      required: true
-    }
-  }]
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.post('/api/users', (req, res) => {
+  const { username } = req.body;
+  const newUser = new User({ username });
+
+  newUser.save((err, user) => {
+    if (err) return res.json({ error: err });
+    res.json({ username: user.username, _id: user._id });
+  });
 });
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return res.json({ error: err });
+    res.json(users);
+  });
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ optionsSuccessStatus: 200 })); 
-
-//routes
-
-//Get all users from the database
-app.get('/api/users/', async (req, res) => {
-  try {
-      const users = await userModel.find({})
-      res.status(200).json(users);
-  } catch (err) {
-      res.status(200).json({ error: err });
-  }
-
-});
-
-//Get the user log
-app.get('/api/users/:_id/logs', async (req, res) => {
-
-  const id = req.params._id;
-  try {
-    const userLog = await userModel.findById(id).exec();
-
-    if (req.query.from || req.query.to) {
-
-      //timestamps for comparing dates
-      let fromDate = new Date(0).getTime();
-      let toDate = new Date().getTime();
-
-      if (req.query.from) {
-        fromDate = new Date(req.query.from).getTime();
-      }
-
-      if (req.query.to) {
-        toDate = new Date(req.query.to).getTime();
-      }
-
-      userLog.log = userLog.log.filter(exercise => {
-        exerciseTimestamp = exercise.date.getTime();
-        if (exerciseTimestamp >= fromDate && exerciseTimestamp<= toDate) return true;
-      })
-
-    }
-
-    if (req.query.limit) {
-      userLog.log = userLog.log.slice(0, req.query.limit);
-    }
-
-    const resultJson = {
-      username: userLog.username,
-      count: userLog.log.length,
-      _id: userLog._id,
-      log: userLog.log.map(({description, duration, date}) => {
-        return {
-          description,
-          duration,
-          date: date.toDateString()
-        }
-      })
-    };
-
-    res.status(200).json(resultJson);
-
-  } catch (err) {
-    res.status(200).json({ error: err });
-  }
-
-});
-
-//Register a user
-app.post('/api/users/', (req, res) => {
-
-  try {
-    const newUser = new userModel({ username: req.body.username, log: [] })
-    // save the object in the database
-    newUser.save((errSaved, userSaved) => {
-      if (errSaved) {
-        res.status(200).json({ err: errSaved });
-      }
-      const { _id, username } = userSaved;
-      res.status(200).json({
-        username,
-        _id
-      });
-    })
-  } catch (err) {
-    res.status(200).json({ error: err });
-  }
-
-});
-
-//Add an exersize to a user
-router.post('/:_id/exercises', (req, res) => {
-  const id = req.params._id;
+app.post('/api/users/:userId/exercises', (req, res) => {
+  const { userId } = req.params;
   const { description, duration, date } = req.body;
 
-  const userIdInput = id;
-  const descriptionInput = description;
-  const durationInput = duration;
-  const dateInput = date || new Date().toISOString();
+  User.findById(userId, (err, user) => {
+    if (err) return res.json({ error: err });
+    if (!user) return res.json({ error: 'User not found' });
 
-  try {
-    userModel.findOneAndUpdate(
-      {
-        _id: userIdInput
-      },
-      {
-        $push: {
-          log: {
-            description: descriptionInput,
-            duration: durationInput,
-            date: dateInput
-          }
-        }
-      },
-      {
-        new: true
-      }
-    ).then(updatedUser => {
+    const newExercise = new Exercise({ userId, description, duration, date });
+    newExercise.save((err, exercise) => {
+      if (err) return res.json({ error: err });
       res.json({
-        username: updatedUser.username,
-        description: updatedUser.log[updatedUser.log.length - 1].description,
-        duration: updatedUser.log[updatedUser.log.length - 1].duration,
-        date: updatedUser.log[updatedUser.log.length - 1].date.toDateString(),
-        _id: updatedUser._id
+        username: user.username,
+        _id: user._id,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date
       });
-    }).catch(err => {
-      res.status(200).json({ error: err });
     });
-  } catch (err) {
-    res.status(200).json({ error: err });
-  }
+  });
 });
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+app.get('/api/users/:userId/logs', (req, res) => {
+  const { userId } = req.params;
+  const { from, to, limit } = req.query;
+
+  User.findById(userId,
+    (err, user) => {
+      if (err) return res.json({ error: err });
+      if (!user) return res.json({ error: 'User not found' });
+  
+      Exercise.find({ userId, date: { $gte: from, $lte: to } })
+        .limit(+limit)
+        .exec((err, exercises) => {
+          if (err) return res.json({ error: err });
+          res.json({
+            username: user.username,
+            _id: user._id,
+            count: exercises.length,
+            log: exercises.map(exercise => ({
+              description: exercise.description,
+              duration: exercise.duration,
+              date: exercise.date.toDateString()
+            }))
+          });
+        });
+    });
+  });
+  
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000');
+  });
+  
